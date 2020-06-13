c3fd45b985162bf95538d76dac8196a42a9c5dac
brandaoti <dannnbrandao@gmail.com>
2020-06-13 19:51:59 -0300

    Criado um MaterialPageRouter, navegar entre telas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -3,22 +3,42 @@ import 'package:flutter/material.dart';
 import '../model/category.dart';
 
 class CategoryItem extends StatelessWidget {
+  // Instancia da classe Category, pegando os atributos passado no construtor
   final Category category;
-
+  // Construtor
   const CategoryItem(this.category);
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return Container();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
+    // Container responsavel pela cor, chamando um array de LinearGradient
+    return InkWell(
+      // Resposavel por chamar a função pra navegar entre telas
+      onTap: () => _selectCategory(context),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          // Passando titulo das refeições
+          child: Text(category.title),
         ),
       ),
-      child: Text(category.title),
     );
   }
 }
