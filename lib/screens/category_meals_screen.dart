2a8f249032fa698785f7425cb12deea1a190b3be
brandaoti <dannnbrandao@gmail.com>
2020-06-14 00:08:25 -0300

    Add model de meal.dart, com enums e add dados no Dummy_category.dart

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -2,18 +2,29 @@ import 'package:flutter/material.dart';
 
 import '../components/meal_items.dart';
 
+import '../data/dummy_category.dart';
+
 import '../model/category.dart';
+import '../model/meal.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Implements ModalRoute
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
+    //
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(meal.id);
+    }).toList();
+
     // Implements build Scaffold, exibir refeições
     return Scaffold(
       appBar: AppBar(
-        title: Text('//category.title,'),
+        title: Text(category.title),
       ),
       body: ListView.builder(
-        itemCount: 1, // Precisa passar as refeições aqui
+        itemCount: categoryMeals.length, // Precisa passar as refeições aqui
         itemBuilder: (context, index) {
           return MealItems();
         },
