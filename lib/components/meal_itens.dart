0a5dd7d46acfaf9ab636b91e13a4be924c6a0ee9
brandaoti <dannnbrandao@gmail.com>
2020-06-15 23:16:47 -0300

    add na stack um positioned pra exibir um texto pré-definido

---
+++ lib/components/meal_itens.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import '../model/meal.dart';
+
+class MealItens extends StatelessWidget {
+  //Instancia da classe
+  final Meal meal;
+
+  //Construtor
+  const MealItens(this.meal);
+
+  // TODO: Criar método pra navegar para tela com os detalhes da Meal
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: add um InkWell e ativar onTap, chamando a função
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.red,
+      margin: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                // Responsavel por gerar as imagens das refeições
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              //TODO: Add o Positioned, pra exibir uma msg
+              Positioned(
+                //
+                left: 10,
+                right: 10,
+                bottom: 20,
+
+                child: Container(
+                  color: Colors.black45,
+                  width: 300,
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  child: Text(
+                    meal.title,
+
+                    // TODO: Add o estilo do MaterialApp
+                    style: TextStyle(
+                      fontFamily: 'Kreon',
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Linha responsavel por exibir tempo, trabalho e preço
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.timer),
+                    Text('${meal.duration} min'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.work),
+                    Text('${meal.complexityText}'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.attach_money),
+                    Text('${meal.costText}'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
