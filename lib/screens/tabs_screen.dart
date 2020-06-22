bf6188aa5264e6f9b2e504c96534d9c5d75eb058
brandaoti <dannnbrandao@gmail.com>
2020-06-21 22:53:16 -0300

    Instanciando MainDrawer() pra ser controlado pela TabsScreen()

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../components/main_drawer.dart';
+
 class TabsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Refeições'),
+      ),
+      drawer: MainDrawer(),
     );
   }
 }
