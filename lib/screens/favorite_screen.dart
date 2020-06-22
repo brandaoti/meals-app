e9bedf7702fc0563620d438d21f98366a9a5c43b
brandaoti <dannnbrandao@gmail.com>
2020-06-22 00:04:21 -0300

    Criado tela de favoritos

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implementando tela pra exibir as refeições favorita
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text('Nenhuma refeição no momento!'),
+        ],
+      ),
+    );
+  }
+}
