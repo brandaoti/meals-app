96ea2180a27736233a441ee5a9053393930630f5
brandaoti <dannnbrandao@gmail.com>
2020-06-22 20:55:14 -0300

    Reconstruindo a classe

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../components/main_drawer.dart';
 
+import '../model/settings.dart';
+
 class SettingsScreen extends StatelessWidget {
+  final settings = Settings();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +15,28 @@ class SettingsScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Configurações'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Text('Configurações'),
+          ),
+
+          //
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                SwitchListTile.adaptive(
+                  title: Text('Sem gluten'),
+                  subtitle: Text('Só exibir refeições sem gluten.'),
+                  value: settings.isGluttenFree,
+                  onChanged: (i) {},
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
