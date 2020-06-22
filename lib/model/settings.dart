ddfd6a02049fb15a57a4bb02d2fe4678d84e227e
brandaoti <dannnbrandao@gmail.com>
2020-06-22 20:46:11 -0300

    Add atributos pra passar nas configurações

---
+++ lib/model/settings.dart
@@ -0,0 +1,13 @@
+class Settings {
+  bool isGluttenFree;
+  bool isLactoseFree;
+  bool isVegan;
+  bool isVegetarian;
+
+  Settings({
+    this.isGluttenFree = false,
+    this.isLactoseFree = false,
+    this.isVegan = false,
+    this.isVegetarian = false,
+  });
+}
